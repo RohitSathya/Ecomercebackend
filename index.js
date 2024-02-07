367e3bea38982b1580fb1f023ec75a7ca0386d13
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-02-07 18:24:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,20 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const RegisterModel = require('./models/Register')
+const express=require('express')
+const cors=require('cors')
+const app=express()
+const mongoose=require('mongoose')
 
-const app = express()
-app.use(cors(
-    {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
+
 app.use(express.json())
+app.use(cors())
 
-mongoose.connect('mongodb+srv://admin:12345@royoapi.3qmdrjq.mongodb.net/ecomeerce?retryWrites=true&w=majority');
-
-
-app.get("/", (req, res) => {
-    res.json("Hello");
-})
-app.post('/register', (req, res) => {
-    const {name, email, password} = req.body;
-    RegisterModel.findOne({email: email})
-    .then(user => {
-        if(user) {
-            res.json("Already have an account")
-        } else {
-            RegisterModel.create({name: name, email: email, password: password})
-            .then(result => res.json(result))
-            .catch(err => res.json(err))
-        }
-    }).catch(err => res.json(err))
+app.use("/product",router)
+app.get("/",async(req,res)=>{
+    res.json("hi")
 })
 
-
-app.listen(5001, () => {
-    console.log("Server is Running")
+                           
+mongoose.connect("mongodb+srv://admin:12345@royoapi.3qmdrjq.mongodb.net/ecomeerce?retryWrites=true&w=majority").then(()=>{
+    console.log('database connected')
+  app.listen(3001,()=>console.log('running'))
+ 
 })

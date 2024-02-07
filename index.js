87ff9a612cbfdd2dd6f05519840b204f10485556
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-02-07 18:15:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const app=express()
 const mongoose=require('mongoose')
 const router=require('./Routes/Routes')
 
+app.use(express.json())
+app.use(cors(
+    {
+        origin:["https://ecomerce-foront.vercel.app"],
+        methods:["POST","GET","PUT","DELETE"]
+    }
+))
 
+app.use('/product',router)
 app.get('/',async(req,res)=>{
     res.end('hi')
 })
@@ -12,7 +20,7 @@ app.get('/',async(req,res)=>{
                            
 mongoose.connect('mongodb+srv://admin:12345@royoapi.3qmdrjq.mongodb.net/ecomeerce?retryWrites=true&w=majority').then(()=>{
     console.log('database connected')
-  app.listen(8080,()=>console.log('running'))
+  app.listen(3000,()=>console.log('running'))
  
 })
 

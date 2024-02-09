3b883e46b5844071e0ee86c90b227359182f0b95
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-02-09 10:07:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express=require('express')
 const cors=require('cors')
 const app=express()
 const mongoose=require('mongoose')
+const router=require('./Routes/Routes')
 
 
 app.use(express.json())
